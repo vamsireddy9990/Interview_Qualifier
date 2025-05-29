7d9a0c6b3e2f6f001871a8fa112e3d8024d2b3e7
vamsireddy9990 <vamsireddyl@gmail.com>
2025-05-29 18:15:36 +0530

    Update main.py with latest changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import PyPDF2
 import pandas as pd
 import io
-
+# test commit
 # --- SETUP ---
 st.set_page_config(page_title="Sun Interview Qualifier", page_icon="☀️", layout="wide")
 
